ba21c421f5a144ad1bf5f191c7f5d33ec658e8c6
Annabel Wilmerding <annabelwilmerding@gmail.com>
2021-03-11 09:47:53 -0800

    crud

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,5 +8,38 @@ class ItemsController < ApplicationController
     @item = Item.find_by(id: params[:id])
   end
 
+  def new
+    @item = Item.new
+  end
+
+  def create
+    item = Item.create(item_params)
+    redirect_to item_path(item)
+  end
+
+  def edit
+    @item = Item.find_by(id: params[:id])
+  end
+
+  def update
+    item = Item.find_by(id: params[:id])
+    item.update(item_params)
+    redirect_to(item_path(item))
+  end
+
+
+  def destroy
+    item = Item.find_by(id: params[:id])
+    item.delete
+    redirect_to items_path
+  end
+
+
+  private
+
+    def item_params
+      params.require(:item).permit(:name)
+    end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # get '/items', to: "items#index"
   # get '/items/:id', to: "items#show"
-  resources(:items, only: [:show, :index])
+  resources(:items)
   # resources(:items)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
