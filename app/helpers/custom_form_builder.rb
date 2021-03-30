65d61361ed0d4ceb2c99cee7324f10ca856c426c
Annabel Wilmerding <annabelwilmerding@gmail.com>
2021-03-30 13:45:55 -0700

    custom form builder fixes

--- app/helpers/custom_form_builder.rb
+++ app/helpers/custom_form_builder.rb
@@ -17,19 +17,23 @@ class CustomFormBuilder < ActionView::Helpers::FormBuilder
   #    end.join("").html_safe
   # end
 
-  def my_fields_for(association)
+  def my_fields_for(association, collection=nil)
    association_attributes_method = "#{association}_attributes="
    object_class_downcase = self.object.class.to_s.downcase
    association_attributes = "#{association}_attributes"
    # binding.pry
     if object.methods.include?(association.to_sym) && object.methods.include?(association_attributes_method.to_sym)
-       object.send(association).each_with_index.map do |a, i|
+
+        collection_for_iteration = collection ||  object.send(association)
+
+       collection_for_iteration.each_with_index.map do |a, i|
            new_object_name = "#{object_class_downcase}[#{association_attributes}][#{i}]"
            new_form_builder = CustomFormBuilder.new(new_object_name, a, @template, @options)
 
          yield(new_form_builder)
+         # binding.pry
          if a.id
-           hidden_field(:id)
+           new_form_builder.hidden_field(:id)
          end
        end
     end.join("").html_safe
